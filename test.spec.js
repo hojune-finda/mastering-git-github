dec6ec5fe94fda8e1085a4a75ce743c7a181e47f
hojune-finda <hojune@finda.co.kr>
2023-06-14 19:53:11 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe('Default Test Set', () => {
+  it('test1 should be passed', () => {
+    console.log('test1 pass')
+  });
+
+  it('test2 should be passed', () => {
+    console.log('test2 pass')
+  });
+});
